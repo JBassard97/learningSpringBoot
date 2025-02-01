732c37a3818de72fcab1038aa4d129147cc97f38
Jbassard <jonathanacciarito@gmail.com>
2025-02-01 13:46:12 -0500

    tried to send a simple fetch from the frontend and it worked well

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,5 +1,20 @@
+import { useEffect } from "preact/hooks";
 
 export default function About() {
+  useEffect(() => {
+    const fetchResponse = async () => {
+      const response = await fetch("/api/users");
+      if (!response.ok) {
+        console.log("Failure");
+      } else {
+        const data = await response.json();
+        console.log(data);
+      }
+    };
+
+    fetchResponse();
+  }, []);
+
   return (
     <div>
       <h1>The About Page</h1>
